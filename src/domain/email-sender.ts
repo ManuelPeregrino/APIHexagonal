28c24632a5990b305b0a2b674fab92236d754aec
Pilgrim-SRC <105981978+ManuelPeregrino@users.noreply.github.com>
2023-05-24 15:32:10 -0600

    deleted another stuff

--- src/domain/email-sender.ts
+++
@@ -1,3 +0,0 @@
-export interface EmailSender {
-  send(email: string, text: string): Promise<void>;
-}
--- src/domain/user-repository.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "./user";
-
-export interface UserRepository {
-  getById(userID: string): Promise<User | null>;
-}
